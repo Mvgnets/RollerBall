52fe9e1e9e892890240c85eeda68073d49fb93b3
Alex Hall <alexander.ian.hall@gmail.com>
2020-01-10 12:57:25 +0000

    Initial Commit

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+	GameObject[] pauseObjects;
+	GameObject[] finishObjects;
+
+	// Use this for initialization
+	void Start()
+	{
+		Time.timeScale = 1;
+
+		pauseObjects = GameObject.FindGameObjectsWithTag("ShowOnPause");            //gets all objects with tag ShowOnPause
+		finishObjects = GameObject.FindGameObjectsWithTag("ShowOnFinish");          //gets all objects with tag ShowOnFinish
+		
+		hidePaused();
+		hideFinished();
+	}
+
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		//uses the p button to pause and unpause the game
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			if (Time.timeScale == 1)
+			{
+				Time.timeScale = 0;
+				showPaused();
+			}
+			else if (Time.timeScale == 0)
+			{
+				Debug.Log("high");
+				Time.timeScale = 1;
+				hidePaused();
+			}
+		}
+	}
+
+
+	//Reloads the Level
+	public void Reload()
+	{
+		SceneManager.LoadScene(Application.loadedLevel);
+	}
+
+	//controls the pausing of the scene
+	public void pauseControl()
+	{
+		if (Time.timeScale == 1)
+		{
+			Time.timeScale = 0;
+			showPaused();
+		}
+		else if (Time.timeScale == 0)
+		{
+			Time.timeScale = 1;
+			hidePaused();
+		}
+	}
+
+	//shows objects with ShowOnPause tag
+	public void showPaused()
+	{
+		foreach (GameObject g in pauseObjects)
+		{
+			g.SetActive(true);
+		}
+	}
+
+	//hides objects with ShowOnPause tag
+	public void hidePaused()
+	{
+		foreach (GameObject g in pauseObjects)
+		{
+			g.SetActive(false);
+		}
+	}
+
+	//shows objects with ShowOnFinish tag
+	public void showFinish()
+	{
+		foreach (GameObject g in finishObjects)
+		{
+			g.SetActive(true);
+		}
+	}
+
+	//hides objects with ShowOnFinish tag
+	public void hideFinish()
+	{
+		foreach (GameObject g in finishObjects)
+		{
+			g.SetActive(false);
+		}
+	}
+
+	//loads inputted level
+	public void LoadLevel()
+	{
+		SceneManager.LoadScene("MainLevel");
+	}
+
+	//loads main menu
+	public void LoadMenu()
+	{
+		SceneManager.LoadScene("MainMenu");
+	}
+
+	public void Quit()
+    {
+		Application.Quit();
+	}
+}
